661585285cce2b5bc5c800ac796e2216b12bc7cc
sneh-soni <ssneh20062003@gmail.com>
2024-02-20 22:02:17 +0530

    multer and cloudinary setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// localFilePath ==> file path on local database
+// can upload files directly to clodinary, but
+// for production grade ==> 1. upload to local database (using multer)
+// 2. upload to cloudinary from database (here) 3. unlink using file system (fs)
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log(response.url, " <==> ", response.secure_url);
+    return response;
+  } catch (error) {
+    // remove local file as upload to cloudinary failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
