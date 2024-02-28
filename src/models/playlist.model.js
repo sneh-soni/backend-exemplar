791729e7e1b624946eb1b5258367babb430e160a
sneh-soni <ssneh20062003@gmail.com>
2024-02-28 12:21:37 +0530

    Added comment, like, tweet and playlist models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Video",
+        },
+      ],
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
